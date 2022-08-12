ddadd81d425cb6d10351d92ab39d688c38d03f51
Arsham <arsham.khodajoo@gmail.com>
2022-08-12 07:17:40 +0330

    update train transforms

--- callme/datasets/transforms.py
+++ callme/datasets/transforms.py
@@ -5,6 +5,7 @@ import random
 
 import numpy as np
 import torch
+import torch.nn.functional as F
 
 
 class ApplyFixAudioLength:
@@ -28,33 +29,37 @@ class ApplyFixAudioLength:
 class ApplyTimeshift:
     """Shifts an audio randomly."""
 
-    def __init__(self, max_shift_seconds=0.2):
-        self.max_shift_seconds = max_shift_seconds
+    def __init__(self, time_shift_ms: int=400, sound_rate :int=16000):
+        self.time_shift_ms= time_shift_ms
+        self.rate = sound_rate
 
     def __call__(self, audio):
+        time_shift = int((self.time_shift_ms * self.rate) / 1000)
+        time_shift_amount = np.random.randint(-time_shift, time_shift)
 
-        sample_rate = 16_000
-        max_shift = (sample_rate * self.max_shift_seconds)
-        shift = random.randint(-max_shift, max_shift)
-        a = -min(0, shift)
-        b = max(0, shift)
-        audio = np.pad(audio.numpy()[0], (a, b), "constant")
-        out = torch.tensor(audio[:len(audio) - a] if a else audio[b:])
-        return out.view((1, *out.size()))
+        if time_shift_amount > 0:
+            time_shift_padding = (time_shift_amount, 0)
+            time_shift_offset = 0
+        else:
+            time_shift_padding = (0, -time_shift_amount)
+            time_shift_offset = -time_shift_amount
 
+        padded_foreground = F.pad(audio, time_shift_padding, 'constant', 0)
+        sliced_foreground = torch.narrow(padded_foreground, 1, time_shift_offset, self.rate)
+        return sliced_foreground
 
 class ApplyBackgroundNoise:
     """apply and concat background noise to 16k wav audio"""
 
-    def __init__(self, bg_dataset, sound_rate: int=16000, volume: float=0.80) -> None:
+    def __init__(self, bg_dataset, sound_rate: int=16000, volume: float=0.60) -> None:
         self.bg_dataset = bg_dataset
         self.rate = sound_rate
         assert volume < 1, "volume too big"
         self.volume = volume
 
     def __call__(self, audio):
-        background_sample = random.choice(self.bg_dataset)
-        assert background_sample.size(1) > self.rate, "background sample too short"
+        background_sample = random.choice(self.bg_dataset)[0]
+        assert len(background_sample) > self.rate, "background sample too short"
         background_offset = random.randint(0, len(background_sample) - self.rate)
         background_clip = background_sample[background_offset:(background_offset+self.rate)]
         background_clip = background_clip.view((1, self.rate))
